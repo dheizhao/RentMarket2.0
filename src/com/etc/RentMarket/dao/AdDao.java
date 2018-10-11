8ddb854153dd04bb50d2789165b8559ad41ba3bf
Administrator <Administrator@stu1003>
2018-10-11 16:55:06 +0800

    数据库与界面的连接2

---
+++ src/com/etc/RentMarket/dao/AdDao.java
@@ -0,0 +1,9 @@
+package com.etc.RentMarket.dao;
+
+import java.util.List;
+
+import com.etc.RentMarket.entity.Ad;
+
+public interface AdDao {
+	public List<Ad> selectAd();
+}
---
+++ src/com/etc/RentMarket/dao/impl/GoodPhoneDaoImpl.java
@@ -0,0 +1,22 @@
+package com.etc.RentMarket.dao.impl;
+
+import java.util.List;
+
+import com.etc.RentMarket.DBUtil.BaseDao;
+import com.etc.RentMarket.dao.PhoneGoodDao;
+import com.etc.RentMarket.entity.PhoneGood;
+
+public class GoodPhoneDaoImpl implements PhoneGoodDao {
+
+	@Override
+	public List<PhoneGood> selectPhone() {
+		// TODO Auto-generated method stub
+		String sql = "select good.goodName,good.goodPrice,good.goodImgAdd from good INNER JOIN goodstype where good.typeId=goodstype.typeId and good.typeId=1";
+		return (List<PhoneGood>) BaseDao.select(sql, PhoneGood.class);
+	}
+
+	
+
+	
+
+}
