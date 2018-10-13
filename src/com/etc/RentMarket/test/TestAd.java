e722301c471a983a6b1da4f8a76b23c301d043cd
Administrator <Administrator@stu1003>
2018-10-13 17:32:05 +0800

    11

--- src/com/etc/RentMarket/test/TestAd.java
+++ src/com/etc/RentMarket/test/TestAd.java
@@ -5,7 +5,7 @@ import com.etc.RentMarket.service.impl.AdServiceImpl;
 public class TestAd {
 	public static void main(String[] args) {
 		AdServiceImpl ad = new AdServiceImpl();
-		System.out.println(ad.selectAd());
+		System.out.println();
 	}
 
 }
